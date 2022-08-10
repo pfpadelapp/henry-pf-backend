57b3d847e276a1ab7926dd14c874238f9d5b2e6f
edRiosJ <eduardo_silk@hotmail.com>
2022-08-10 16:49:11 -0500

    Pasarela terminada

--- routes/payment/index.js
+++ routes/payment/index.js
@@ -5,57 +5,76 @@ const { CLIENT, SECRET } = process.env;
 const PAYPAL_API = 'https://api-m.sandbox.paypal.com';
 const auth = {user: CLIENT, pass: SECRET};
 
-const createPayment = ( req, res ) =>
-{
-    const body =
-    {
-        intent: 'CAPTURE',
-        purchase_units:
-        [
-            {
-                amount:
-                {
-                    currency_code: 'USD',
-                    value: '150'
-                }
-            }
-        ],
-        application_context:
-        {
-            brand_name: 'Padel Field',
-            landing_page: 'NO_PREFERENCE',
-            user_action: 'PAY_NOW',
-            return_url: 'http://localhost:3000/payment/executePayment',
-            cancel_url: 'http://localhost:5173/home'
-        }
-    };
-
-    request.post(`${PAYPAL_API}/v2/checkout/orders`,{
-        auth,
-        body,
-        json: true
-    }, (_err, response) => {
-        res.send(response.body)
-    })
-}
-
-const executePayment = (req, res) =>
-{
-    const token = req.query.token;
-
-    request.post(`${PAYPAL_API}/v2/checkout/orders/${token}/capture`,
-    {
-        auth,
-        body: {},
-        json: true
-    }, (_err, response) =>
-    {
-        res.send(response.body);
-    })
-}
-
 module.exports = async function (fastify, opts)
 {
-    fastify.post('/createPayment', createPayment);
-    fastify.get('/executePayment', executePayment);
+    fastify.post('/createPayment', (req, res)=>
+    {
+        try
+        {
+            const {idField, cost} = req.body;
+            const body =
+            {
+                intent: 'CAPTURE',
+                purchase_units:
+                [
+                    {
+                        amount:
+                        {
+                            currency_code: 'USD',
+                            value: cost
+                        }
+                    }
+                ],
+                application_context:
+                {
+                    brand_name: 'Padel Field',
+                    landing_page: 'NO_PREFERENCE',
+                    user_action: 'PAY_NOW',
+                    return_url: `http://localhost:5173/detail/${idField}`,
+                    cancel_url: `http://localhost:5173/detail/${idField}`
+                }
+            };
+
+            request.post(`${PAYPAL_API}/v2/checkout/orders`,
+            {
+                auth,
+                body,
+                json: true
+            },
+            (_err, response) =>
+            {
+                if(!_err) res.send(response.body.links[1].href);
+                else res.send(response.body);
+            })
+        }
+        catch(e)
+        {
+            return e;
+        }
+    });
+
+    fastify.get('/executePayment', (req, res) =>
+    {
+        try
+        {
+            const token = req.query.token;
+
+            request.post(`${PAYPAL_API}/v2/checkout/orders/${token}/capture`,
+            {
+                auth,
+                body: {},
+                json: true
+            },
+            (_err, response) =>
+            {
+                // res.send(response.body);
+                if(!_err) res.send({msg: 'Payment has been succesfull'});
+                else res.send({err: 'Payment has been rejected'});
+            })
+        }
+        catch(e)
+        {
+            return e;
+        }
+    });
 }
