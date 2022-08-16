3b3698ecfccbddb9c6a433c54f66307f31f94653
feticheX <dfcmera@gmail.com>
2022-08-15 23:06:41 -0500

    make it better

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-const routes = require('./app/routes/root')
+const routes = require('./routes/root')
 const cors = require('cors')
 // const path = require('path')
 
