71b7f653ebb6ca546d830b3de7e5f127c00d85fc
wacho91 <criskol.71@gmail.com>
2022-08-17 14:06:10 -0500

    Search admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,7 @@
 const Admin = require('../models/Admin')
 const bcrypt = require('bcrypt')
+const User = require('../models/User')
+const Owner = require('../models/Owner')
 
 async function getAllAdmins() {
   try {
@@ -19,6 +21,38 @@ async function getAdminById(adminId) {
     }
   }
 
+async function searchUsers(username) {
+
+    try {
+      const users = await User.find({ username: { $regex: username, $options: 'i' }})
+      const userFilter = users.map(u => {
+        return {
+          _id: u._id,
+          username: u.username,
+          email: u.email,
+        }
+      })
+      return userFilter
+    } catch (e) {
+      return e
+    }
+  }
+
+async function searchOwner(username) {
+  try{
+    const owner = await Owner.find({ username: { $regex: username, $options: 'i' }})
+    const ownerFilter = owner.map(o => {
+      return {
+        _id: o._id,
+        username: o.username,
+        email: o.email,
+      }
+    } )
+    return ownerFilter
+  }catch(e){
+    return e
+  }
+}
 
   
 async function createAdmin(name, email, username, password ) {
@@ -71,5 +105,7 @@ module.exports = {
     getAllAdmins,
     createAdmin,
     deleteAdminById,
-    updatedAdmin
+    updatedAdmin,
+    searchUsers,
+    searchOwner
   }
